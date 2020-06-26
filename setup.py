d008ec6fb9db51fec43be5383653582a73fad18b
Robin Péricé <robin.perice@protonmail.com>
2020-06-26 15:08:18 +0200

    ready to deploy v1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
     name='mediapart_parser',
     packages=['mediapart_parser'],
-    version='1.0.0-beta.2',
+    version='1.0.0',
     license='MIT',
     description='Provide tools to parse and download Mediapart articles as PDF.',
     author='Robin Perice',
     author_email='robin.perice@protonmail.com',
     url='https://github.com/r0perice/mediapart-parser',
-    download_url='https://github.com/r0perice/mediapart-parser/archive/1.0.0-beta.2.tar.gz',
+    download_url='https://github.com/r0perice/mediapart-parser/archive/1.0.0.tar.gz',
     install_requires=[
           'feedparser',
           'beautifulsoup4',
