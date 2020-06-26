9f41be7598000c52bab754323f985c06878044ab
Robin Péricé <robin.perice@protonmail.com>
2020-06-26 10:06:10 +0200

    Update tools, code quality

--- mediapart_parser/mediapart_parser.py
+++ mediapart_parser/mediapart_parser.py
@@ -5,7 +5,7 @@ import os
 from bs4 import BeautifulSoup
 from requests import Session
 
- 
+
 class MediapartParser:
 
     mediapart_base_pdf_url = "https://www.mediapart.fr/tools/pdf/"
@@ -13,52 +13,56 @@ class MediapartParser:
     mediapart_user = ""
     mediapart_pwd = ""
 
-    def __init__(self, user, pwd): 
+    def __init__(self, user, pwd):
         self.mediapart_user = user
         self.mediapart_pwd = pwd
 
     def get_rss_entries(self):
-        mediapart_parsed_rss = feedparser.parse("https://www.mediapart.fr/articles/feed")
+        mediapart_parsed_rss = feedparser.parse(
+            "https://www.mediapart.fr/articles/feed")
         last_articles = mediapart_parsed_rss.entries
         return last_articles
 
     def get_last_articles_titles(self):
         titles = []
-        last_articles = self.get_rss_entries()  
+        last_articles = self.get_rss_entries()
         for article in last_articles:
             titles.append(article.title)
         return titles
-    
+
     def get_last_categories(self):
         categories = []
-        last_articles = self.get_rss_entries()  
+        last_articles = self.get_rss_entries()
         for article in last_articles:
             categories.append(article.tags[0].term)
         return categories
 
     def get_last_articles_links(self):
         all_links = []
-        last_articles = self.get_rss_entries()  
+        last_articles = self.get_rss_entries()
         for article in last_articles:
             for link in article.links:
                 all_links.append(link.href)
         return all_links
-     
+
     def get_last_full_articles_links(self):
         full_article_suffix = "?onglet=full"
         all_links = []
-        last_articles = self.get_rss_entries()  
+        last_articles = self.get_rss_entries()
         for article in last_articles:
             for link in article.links:
                 all_links.append(link.href + full_article_suffix)
         return all_links
 
-    def get_article_id(self, article_url): 
-        page = requests.get(article_url,auth=(self.mediapart_user, self.mediapart_pwd))
+    def get_article_id(self, article_url):
+        page = requests.get(
+            article_url,
+            auth=(self.mediapart_user, self.mediapart_pwd))
         soup = BeautifulSoup(page.text, "html.parser")
-        result = soup.find("div", {"class":"sub-header accur8-desktop accur8-tablet accurWidth-desktop accurWidth-tablet"})
+        result = soup.find("div", {"class": "sub-header accur8-desktop\
+            accur8-tablet accurWidth-desktop accurWidth-tablet"})
         return result["data-nid"]
-    
+
     def get_all_pdf_links(self):
         pdf_link_preffix = "https://www.mediapart.fr/tools/pdf/"
         pdf_links = []
@@ -76,8 +80,9 @@ class MediapartParser:
         url = self.mediapart_base_pdf_url + str(article_id)
 
         s = Session()
-        login_data = {"name":self.mediapart_user,"password":self.mediapart_pwd,"op":"Se+connecter"}
-        s.post("https://www.mediapart.fr/login_check",login_data)
+        login_data = {"name": self.mediapart_user,
+                      "password": self.mediapart_pwd, "op": "Se+connecter"}
+        s.post("https://www.mediapart.fr/login_check", login_data)
         page = s.get(url)
         f = open(final_file_path, 'wb')
         f.write(page.content)
@@ -85,7 +90,3 @@ class MediapartParser:
 
         while not os.path.exists(final_file_path):
             time.sleep(1)
-
-        
-
-
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-    name='mediapart_bot_core',
-    packages=['mediapart_bot_core'],
-    version='1.0.0-beta.1',
+    name='mediapart_parser',
+    packages=['mediapart_parser'],
+    version='1.0.0-beta.2',
     license='MIT',
-    description='Provide tools to parser and download Mediapart articles as PDF.',
+    description='Provide tools to parse and download Mediapart articles as PDF.',
     author='Robin Perice',
     author_email='robin.perice@protonmail.com',
-    url='https://github.com/r0perice/mediapart-bot-core',
-    download_url='https://github.com/r0perice/mediapart-bot-core/archive/1.0.0-beta.1.tar.gz',
+    url='https://github.com/r0perice/mediapart-parser',
+    download_url='https://github.com/r0perice/mediapart-parser/archive/1.0.0-beta.2.tar.gz',
     install_requires=[
           'feedparser',
           'beautifulsoup4',
