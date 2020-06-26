2fad0db47a34daf5259fe336b853f55478f9d79e
Robin Péricé <robin.perice@protonmail.com>
2020-06-26 15:23:09 +0200

    add Readme for Pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='mediapart_parser',
     packages=['mediapart_parser'],
     version='1.0.0',
     license='MIT',
     description='Provide tools to parse and download Mediapart articles as PDF.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Robin Perice',
     author_email='robin.perice@protonmail.com',
     url='https://github.com/r0perice/mediapart-parser',
