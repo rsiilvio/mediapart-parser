5efa4042a7fde3a333902ce8003aceb9db88e224
Robin Péricé <robin.perice@protonmail.com>
2020-06-26 15:27:39 +0200

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='mediapart_parser',
     packages=['mediapart_parser'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='Provide tools to parse and download Mediapart articles as PDF.',
     long_description=long_description,
@@ -17,7 +17,7 @@ setup(
     author='Robin Perice',
     author_email='robin.perice@protonmail.com',
     url='https://github.com/r0perice/mediapart-parser',
-    download_url='https://github.com/r0perice/mediapart-parser/archive/1.0.0.tar.gz',
+    download_url='https://github.com/r0perice/mediapart-parser/archive/1.0.1.tar.gz',
     install_requires=[
           'feedparser',
           'beautifulsoup4',
